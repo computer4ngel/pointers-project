0479c1498680b7567e333629ccc39d858b9db57b
computer4ngel <aodomleo@gmail.com>
2023-02-15 22:07:56 -0800

    Task 2/3 done.

--- pointers.h
+++ pointers.h
@@ -1,10 +1,15 @@
 #ifndef POINTERS_H
 #define POINTERS_H
+#include <string>
 
 void readWrite();
 
 void answerIntegerQuestions(const int num, bool * isMultiple, bool * isSumEven, bool * isPrime);
 
 bool primeCheck(int n);
+
+void initializeLottery();
+
+bool findWinningLotteryTicket(const int winningTicketNum, const int * playerNumbers, const int numPlayerNumbers[10]);
 
 #endif
